3ee28884e5543a9de9495c7d43f3adbac04431cc
Ronald Dias <ronaldsilva850@gmail.com>
2026-03-11 15:55:32 -0300

    fix: '.gitignore' atualizado

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+
+
+export default defineConfig(async () => ({
+  plugins: [vue(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+}));
